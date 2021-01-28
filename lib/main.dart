93d6d086dd7f8b16305cadb2f8122d7a02bcbe9b
Mephrine <dthwrld@gmail.com>
2021-01-28 23:24:17 +0900

    Start screen changed to MainView

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class Main extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialMain();
+    return MainView();
   }
 }
 
