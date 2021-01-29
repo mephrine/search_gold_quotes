446559a85f3e3cb521cee3bdf9289de98643de93
Mephrine <dthwrld@gmail.com>
2021-01-29 13:39:49 +0900

    2초 딜레이 후 메인스크린으로 변경하는 부분 수정 필요

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:search_gold_quotes/main_view.dart';
 
@@ -14,14 +12,15 @@ class Splash extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    new Future.delayed(
+      const Duration(seconds: 2),
+            () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => MainScreen()))
+        // () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MainScreen()))
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 2), () =>
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MainScreen()))
-    );
-
     var centerText = Text("스플래시 화면");
 
     return  MaterialApp(
