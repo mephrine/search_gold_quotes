6b1d8910b819d5c2ad91a6733a040b75cd4f1be5
Mephrine <dthwrld@gmail.com>
2021-01-31 21:25:54 +0900

    execute 메소드 작성

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:search_gold_quotes/core/error/failures.dart';
+import 'package:search_gold_quotes/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:search_gold_quotes/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:meta/meta.dart';
 
 class GetConcreteNumberTrivia {
   final NumberTriviaRepository numberTriviaRepository;
 
   GetConcreteNumberTrivia(this.numberTriviaRepository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+      @required int number
+  }) async {
+    return await numberTriviaRepository.getConcreteNumberTrivia(number);
+  }
 }
