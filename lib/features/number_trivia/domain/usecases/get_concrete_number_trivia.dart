bfed6cf4b0508cb326775123b6eded42f60bf18d
Mephrine <dthwrld@gmail.com>
2021-01-31 20:13:31 +0900

    usecase에서 필요로 했던 클래스 생성

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:search_gold_quotes/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository numberTriviaRepository;
+
+  GetConcreteNumberTrivia(this.numberTriviaRepository);
+}
