daa9810368eab22b99a4160ff00b8734c537b952
Mephrine <dthwrld@gmail.com>
2021-01-31 19:45:43 +0900

    NumberTrivia Entities 부분 작업. Test코드 작성 전.

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  List<dynamic> properties = const<dynamic>[];
+  Failure(this.properties);
+
+  @override
+  List<dynamic> get props {
+    return this.properties;
+  }
+}
---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  NumberTrivia({
+    @required this.text,
+    @required this.number
+  });
+
+  @override
+  List<Object> get props => [text, number];
+
+}
