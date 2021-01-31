9f528d3f998a1f53b12fcaad5279f51e8d0a16db
Mephrine <dthwrld@gmail.com>
2021-01-31 22:27:38 +0900

    NumberTriviaModel real / test 파일 생성

---
+++ test/features/number_trivia/data/number_trivia_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final testNumberTriviaModel = NumberTriviaModel(number: 1, text: "Test text");
+
+  test(
+    "should be a subclass of NumberTrivia entity",
+        () async {
+
+    },
+  );
+}
