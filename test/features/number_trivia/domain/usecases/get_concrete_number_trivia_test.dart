10fe22414700ddf651cb316d53a742bf43539683
Mephrine <dthwrld@gmail.com>
2021-01-31 21:26:41 +0900

    should get trivia for the number from the repository부분 테스트 코드 작성

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,6 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:search_gold_quotes/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:search_gold_quotes/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:search_gold_quotes/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {
 
@@ -13,5 +16,23 @@ void main() {
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final testNumber = 1;
+  final testNumberTrivia = NumberTrivia(number: 1, text: "test");
+  
+  test("should get trivia for the number from the repository", () async {
+    // arrange
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+        .thenAnswer((_) async => Right(testNumberTrivia));
+
+    // act
+    final result = await usecase.execute(number: testNumber);
+
+    // assert
+    expect(result, Right(testNumberTrivia));
+    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(testNumber));
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+
   });
 }
