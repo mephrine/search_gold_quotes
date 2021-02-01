1b751793da73199fbd87678834cdb4543f7d5e52
Mephrine <dthwrld@gmail.com>
2021-02-01 13:09:36 +0900

    api용 data_source 추가

---
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:search_gold_quotes/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaRemoteDataSource {
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
+  Future<NumberTriviaModel> getRandomNumberTrivia();
+}
