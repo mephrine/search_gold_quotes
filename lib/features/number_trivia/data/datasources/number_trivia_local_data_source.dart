962e3757e34d24d078e9585b60fef71f685f34cd
Mephrine <dthwrld@gmail.com>
2021-02-01 13:09:46 +0900

    local용 data_source 추가

---
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:search_gold_quotes/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  Future<NumberTriviaModel> getLastNumberTrivia();
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
