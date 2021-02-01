50cbf2e2e3554eafca1eab77211af61d24348683
Mephrine <dthwrld@gmail.com>
2021-02-01 13:09:07 +0900

    failure 서버 오류 처리 관련 클래스 추가

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,11 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:search_gold_quotes/core/error/exceptions.dart';
 
 abstract class Failure extends Equatable {
   List<dynamic> properties = const<dynamic>[];
-  Failure(this.properties);
+  Failure([this.properties]);
 
   @override
   List<dynamic> get props {
     return this.properties;
   }
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(List properties) : super(properties);
+}
+
+class CacheFailure extends Failure {
+  CacheFailure(List properties) : super(properties);
+
 }
