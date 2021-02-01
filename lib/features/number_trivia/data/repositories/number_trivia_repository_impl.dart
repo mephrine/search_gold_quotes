b1e6a37a4698db2a563ec84a052d9f7481361929
Mephrine <dthwrld@gmail.com>
2021-02-01 13:08:19 +0900

    테스트 코드 작성 완료했던 NumberTriviaRepository 리얼 클래스 작성

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:search_gold_quotes/core/error/failures.dart';
+import 'package:search_gold_quotes/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:search_gold_quotes/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
+  
+}
