8feba41b383dafe55bb1734452645a0aed21348d
Mephrine <dthwrld@gmail.com>
2021-02-04 14:00:08 +0900

    URL 및 header 검증 테스트코드 작성

--- test/features/number_trivia/data/datasources/number_trivia_remote_datasource_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_remote_datasource_test.dart
@@ -19,8 +19,24 @@ void main() {
   NumberTriviaRemoteDataSourceImpl dataSource;
   MockHttpClient mockHttpClient;
 
-  setUp((){
+  setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = NumberTriviaRemoteDataSourceImpl(httpClient: mockHttpClient);
+  });
+
+  group('GetConcreteNumberTrivia', () {
+    final testNumber = 1;
+    final numberTriviaModel = fixture('trivia.json');
+    test('''should perform a GET request on a URL with number
+    being the endpoint and with application/json header''',
+            () {
+          // arrange
+          when(mockHttpClient.get(any, headers: anyNamed('headers')))
+              .thenAnswer((_) async => Response(fixture('trivia.json'), 200));
+          // act
+          dataSource.getConcreteNumberTrivia(testNumber);
+          // assert
+          verify(mockHttpClient.get('http://numbersapi.com/$testNumber', headers: {'Content-Type': 'application/json'}));
+        });
   });
 }
