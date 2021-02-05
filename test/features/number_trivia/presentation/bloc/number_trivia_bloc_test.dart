5d579003c30b08832e684448691fedb16b981947
Mephrine <dthwrld@gmail.com>
2021-02-05 23:05:15 +0900

    Invaild에러 발생 시, Empty -> Error 스테이트로 변경되는 테스트 코드 작성

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -7,8 +7,11 @@ import 'package:search_gold_quotes/features/number_trivia/domain/usecases/get_co
 import 'package:search_gold_quotes/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:search_gold_quotes/features/number_trivia/presentation/number_trivia/number_trivia_bloc.dart';
 
-class MockGetConcreteNumberTrivia extends Mock implements GetConcreteNumberTrivia {}
+class MockGetConcreteNumberTrivia extends Mock
+    implements GetConcreteNumberTrivia {}
+
 class MockGetRandomNumberTrivia extends Mock implements GetRandomNumberTrivia {}
+
 class MockInputConverter extends Mock implements InputConverter {}
 
 void main() {
@@ -30,21 +33,38 @@ void main() {
   test('initialState should be Empty ', () async {
     // assert
     expect(numberTriviaBloc.initialState, equals(Empty()));
-   });
+  });
 
   group('GetTriviaForConcreteNumber', () {
     final testNumberString = '1';
     final testParsedNumber = 1;
     final numberTrivia = NumberTrivia(number: 1, text: 'TEST TEXT');
-    test('should call the inputConverter to validate and convert the string to an unsigned integer.', () async {
-        // arrange
-        when(mockInputConverter.stringToUnsignedInteger(any))
-            .thenReturn(Right(testParsedNumber));
-        // act
-        numberTriviaBloc.add(GetTriviaForConcreteNumber(testNumberString));
-        await untilCalled(mockInputConverter.stringToUnsignedInteger(any));
-        // assert
-        verify(mockInputConverter.stringToUnsignedInteger(testNumberString));
-     });
+    test(
+        'should call the inputConverter to validate and convert the string to an unsigned integer.',
+        () async {
+      // arrange
+      when(mockInputConverter.stringToUnsignedInteger(any))
+          .thenReturn(Right(testParsedNumber));
+      // act
+      numberTriviaBloc.add(GetTriviaForConcreteNumber(testNumberString));
+      await untilCalled(mockInputConverter.stringToUnsignedInteger(any));
+      // assert
+      verify(mockInputConverter.stringToUnsignedInteger(testNumberString));
+    });
+
+    test('should emit [Error] when the input is invalid.', () async {
+      // arrange
+      when(mockInputConverter.stringToUnsignedInteger(any))
+          .thenReturn(Left(InvalidInputFailure()));
+      // act
+      numberTriviaBloc.add(GetTriviaForConcreteNumber(testNumberString));
+      final expected = [
+        Empty(),
+        Error(message: INVALID_INPUT_FAILURE_MESSAGE)
+      ];
+
+      // assert
+      expectLater(numberTriviaBloc.state, emitsInOrder(expected));
+    });
   });
 }
