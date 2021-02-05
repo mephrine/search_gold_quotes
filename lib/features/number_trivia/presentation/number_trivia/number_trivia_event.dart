54f801db290ce6e5e10e8fdbbac220442d0d9df2
Mephrine <dthwrld@gmail.com>
2021-02-05 21:40:02 +0900

    이벤트 작성

--- lib/features/number_trivia/presentation/number_trivia/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/number_trivia/number_trivia_event.dart
@@ -2,3 +2,13 @@ part of 'number_trivia_bloc.dart';
 
 @immutable
 abstract class NumberTriviaEvent {}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString);
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {
+  
+}
