f6e37fb1802874868871768552212ffa37879878
Mephrine <dthwrld@gmail.com>
2021-02-10 17:37:47 +0900

    상속 관계 수정

--- lib/features/price/domain/entities/gold.dart
+++ lib/features/price/domain/entities/gold.dart
@@ -1,8 +1,18 @@
 import 'package:equatable/equatable.dart';
-
+import 'package:meta/meta.dart';
 import 'jewelry.dart';
 
 class Gold extends Jewelry {
+  String date;
+  String day;
+  String price;
+
+
+  Gold({
+    @required this.date,
+    @required this.day,
+    @required this.price});
+
   @override
   List<Object> get props => [date, day, price];
 }
--- lib/features/price/domain/entities/jewelry.dart
+++ lib/features/price/domain/entities/jewelry.dart
@@ -1,8 +1,4 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-abstract class Jewelry extends Equatable {
-  String date;
-  String day;
-  String price;
-}
+abstract class Jewelry extends Equatable {}
