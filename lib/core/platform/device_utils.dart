2e428514a9f0ebcec87ccf86f361df9812674e74
Mephrine <dthwrld@gmail.com>
2021-02-13 20:10:20 +0900

    OS 체크 클래스 => 메소드로 변경

--- lib/core/platform/device_utils.dart
+++ lib/core/platform/device_utils.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/foundation.dart' as foundation;
 
-class DeviceUtils {
-static bool get isAndroid =>
+bool get isAndroid =>
     foundation.defaultTargetPlatform == foundation.TargetPlatform.android;
-}
