5bf8e8e82396187c36a0eab3b8c2cd4c52aca4a4
Mephrine <dthwrld@gmail.com>
2021-02-13 20:09:58 +0900

    toast 위젯 추가

---
+++ lib/core/values/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final Color accentColor = Color(0xFF53ac6d);
+final Color accentLightColor = Color(0xFF53ac6d);
+
+final Color primaryColor = Color(0xFFd7dae0);
+final Color primaryColorDark = Color(0xFFabb2bf);
+
+final Color backgroundColor = Color(0xFF21252b);
+
+
+final Color toastBackgroundColor = Colors.black38;
+final Color toastTextColor = Colors.white;
---
+++ lib/core/values/dimens.dart
@@ -0,0 +1,10 @@
+final double margin = 15.0;
+final double spacing = 8;
+
+final double fontTextTitle = 22.0;
+final double fontTextBig = 18.0;
+final double fontText = 16.0;
+final double fontTextSmall = 14.0;
+final double fontTextSmaller = 12.0;
+
+final double fontButton = 14.0;
