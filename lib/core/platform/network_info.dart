5fed673e2a79d58f91a97dc50e7b1edc40933e07
Mephrine <dthwrld@gmail.com>
2021-02-13 21:55:11 +0900

    네트워크 상태 체크 라이브러리 수정으로 인한 테스트코드 및 해당 코드 수정

--- lib/core/platform/network_info.dart
+++ lib/core/platform/network_info.dart
@@ -1,14 +1,18 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:connectivity/connectivity.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+  Stream<ConnectivityResult> get connectionListener;
 }
 
 class NetworkInfoImpl implements NetworkInfo {
-  final DataConnectionChecker dataConnectionChecker;
+  final Connectivity connectivity;
 
-  NetworkInfoImpl(this.dataConnectionChecker);
+  NetworkInfoImpl(this.connectivity);
 
   @override
-  Future<bool> get isConnected => dataConnectionChecker.hasConnection;
+  Future<bool> get isConnected async => await connectivity.checkConnectivity() != ConnectivityResult.none;
+
+  @override
+  Stream<ConnectivityResult> get connectionListener => connectivity.onConnectivityChanged;
 }
--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,33 +1,47 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:dartz/dartz.dart';
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:mockito/mockito.dart';
 import 'package:search_gold_quotes/core/platform/network_info.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+class MockDataConnectivity extends Mock implements Connectivity {}
 
 void main() {
-  MockDataConnectionChecker mockDataConnectionChecker;
+  MockDataConnectivity mockDataConnectionChecker;
   NetworkInfoImpl networkInfo;
 
   setUp(() {
-    mockDataConnectionChecker = MockDataConnectionChecker();
+    mockDataConnectionChecker = MockDataConnectivity();
     networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
   });
 
   group('isConnected', () {
     test(
-        'should forward the call to DataConnectionChecker.hasConnection', () async {
+        'should forward the call to Connectivity.checkConnectivity in cellular network', () async {
       // arrange
-      final testHasConnection = Future.value(true);
-      when(mockDataConnectionChecker.hasConnection)
+      final testHasConnection = Future.value(ConnectivityResult.mobile);
+      when(mockDataConnectionChecker.checkConnectivity())
           .thenAnswer((_) => testHasConnection);
       // act
       final result = networkInfo.isConnected;
 
       // assert
-      verify(mockDataConnectionChecker.hasConnection);
-      expect(result, testHasConnection);
+      verify(mockDataConnectionChecker.checkConnectivity());
+      expect(result, Future.value(true));
+    });
+
+    test(
+        'should forward the call to Connectivity.checkConnectivity in cellular wifi', () async {
+      // arrange
+      final testHasConnection = Future.value(ConnectivityResult.wifi);
+      when(mockDataConnectionChecker.checkConnectivity())
+          .thenAnswer((_) => testHasConnection);
+      // act
+      final result = networkInfo.isConnected;
+
+      // assert
+      verify(mockDataConnectionChecker.checkConnectivity());
+      expect(result, Future.value(true));
     });
   });
 }
