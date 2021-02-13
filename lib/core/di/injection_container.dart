286ceb42044dad19b09ff105f0021dde8b0f314d
Mephrine <dthwrld@gmail.com>
2021-02-13 22:25:59 +0900

    라이브러리 변경으로 인한 DI 수정

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,7 +1,6 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:search_gold_quotes/core/platform/network_info.dart';
-import 'package:search_gold_quotes/core/presentation/utils/input_converter.dart';
 import 'package:search_gold_quotes/app/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:search_gold_quotes/app/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:search_gold_quotes/app/number_trivia/data/repositories/number_trivia_repository_impl.dart';
@@ -9,8 +8,9 @@ import 'package:search_gold_quotes/app/number_trivia/domain/repositories/number_
 import 'package:search_gold_quotes/app/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:search_gold_quotes/app/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:search_gold_quotes/app/number_trivia/presentation/number_trivia/number_trivia_bloc.dart';
+import 'package:search_gold_quotes/core/platform/network_info.dart';
+import 'package:search_gold_quotes/core/presentation/utils/input_converter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
 
 // Service Locator
 final sl = GetIt.instance;
@@ -52,8 +52,8 @@ Future<void> init() async {
   //! External
   final sharePreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharePreferences);
-  sl.registerLazySingleton(() => http.Client());
-  sl.registerLazySingleton(() => DataConnectionChecker());
+  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(() => Connectivity());
 }
 
 void initFeatures() {}
