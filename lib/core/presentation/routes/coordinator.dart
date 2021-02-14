c0d09017042ff197d84d52009d1811869cf4c719
Mephrine <dthwrld@gmail.com>
2021-02-15 00:08:54 +0900

    router 추가

--- lib/core/presentation/routes/coordinator.dart
+++ lib/core/presentation/routes/coordinator.dart
@@ -1,13 +1,20 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:auto_route_generator/route_config_resolver.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:search_gold_quotes/app/presentation/pages/intro/splash_page.dart';
 import 'package:search_gold_quotes/app/presentation/pages/main/main_page.dart';
 import 'package:search_gold_quotes/app/presentation/pages/setting/setting_page.dart';
+import 'coordinator.gr.dart';
 
 @AdaptiveAutoRouter(routes: <AutoRoute>[
   AdaptiveRoute(page: SplashPage, initial: true),
-  AdaptiveRoute(page: MainPage),
-  AdaptiveRoute(page: SettingPage, fullscreenDialog: true),
+  AdaptiveRoute(page: MainPage, children: <AutoRoute> [
+    AdaptiveRoute(page: SettingPage, fullscreenDialog: true),
+  ]),
 ])
 
-class $Router {}
+class $Coordinator{}
+
+extension RouterNavigationHelper on ExtendedNavigatorState {
+  Future pushMainPage() => push(Routes.mainPage);
+}
