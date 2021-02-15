e2f1dfa0ee76bbd700d4b7ae55a82464d82929b6
Mephrine <dthwrld@gmail.com>
2021-02-15 23:32:23 +0900

    버전 정보 가져오기 dependency 추가 및 DeviceUtils에 추가

--- lib/core/platform/device_utils.dart
+++ lib/core/platform/device_utils.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/foundation.dart' as foundation;
+import 'package:package_info/package_info.dart';
 
 bool get isAndroid =>
     foundation.defaultTargetPlatform == foundation.TargetPlatform.android;
+
+Future<String> get version async {
+  final packageInfo = await PackageInfo.fromPlatform();
+  return packageInfo.version;
+}
