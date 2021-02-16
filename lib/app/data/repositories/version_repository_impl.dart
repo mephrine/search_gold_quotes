39bb561d737676e734746b79ac014f1605ce41a6
Mephrine <dthwrld@gmail.com>
2021-02-16 13:46:19 +0900

    repositoryImpl 테스트코드 작성

--- lib/app/data/repositories/version_repository_impl.dart
+++ lib/app/data/repositories/version_repository_impl.dart
@@ -3,18 +3,30 @@ import 'package:meta/meta.dart';
 import 'package:search_gold_quotes/app/data/datasources/version_remote_data_source.dart';
 import 'package:search_gold_quotes/app/domain/entities/version_info.dart';
 import 'package:search_gold_quotes/app/domain/repositories/version_repository.dart';
+import 'package:search_gold_quotes/core/error/exceptions.dart';
 import 'package:search_gold_quotes/core/error/failures.dart';
+import 'package:search_gold_quotes/core/platform/network_info.dart';
 
 class VersionRepositoryImpl extends VersionRepository {
-  VersionRemoteDataSource remoteDataSource;
+  final VersionRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
 
   VersionRepositoryImpl({
-    @required this.remoteDataSource
+    @required this.remoteDataSource,
+    @required this.networkInfo
   });
 
   @override
   Future<Either<Failure, VersionInfo>> getVersionInfo() async {
-    return Right(await remoteDataSource.getVersionInfo());
+    final isConnected = await networkInfo.isConnected;
+    if (isConnected) {
+      try {
+        final result = await remoteDataSource.getVersionInfo();
+        return Right(result);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    }
+    return Left(ServerFailure());
   }
-
 }
--- test/app/data/repositories/version_repository_impl_test.dart
+++ test/app/data/repositories/version_repository_impl_test.dart
@@ -5,21 +5,31 @@ import 'package:search_gold_quotes/app/data/datasources/version_remote_data_sour
 import 'package:search_gold_quotes/app/data/models/version_info_model.dart';
 import 'package:search_gold_quotes/app/data/repositories/version_repository_impl.dart';
 import 'package:search_gold_quotes/core/error/exceptions.dart';
+import 'package:search_gold_quotes/core/error/failures.dart';
+import 'package:search_gold_quotes/core/platform/network_info.dart';
 
 
 class MockVersionRemoteDataSource extends Mock implements VersionRemoteDataSource {}
+class MockNetworkInfo extends Mock implements NetworkInfo {}
 
 void main() {
   VersionRepositoryImpl repository;
+  MockNetworkInfo networkInfo;
   MockVersionRemoteDataSource remoteDataSource;
   
   setUp(() {
+    networkInfo = MockNetworkInfo();
     remoteDataSource = MockVersionRemoteDataSource();
-    repository = VersionRepositoryImpl(remoteDataSource: remoteDataSource);
+    repository = VersionRepositoryImpl(networkInfo: networkInfo,remoteDataSource: remoteDataSource);
   });
   
-  group('getVersionInfo is successful', () {
+  group('getVersionInfo is online', () {
     final versionInfoModel = VersionInfoModel(latestVersion: "1.0.0", forceUpdate: "Y");
+    setUp(() {
+      when(networkInfo.isConnected)
+          .thenAnswer((_) async => true);
+    });
+
     test('should return versionInfo when the call to version info datasource is successful', () async {
       // arrange
       when(remoteDataSource.getVersionInfo())
@@ -31,20 +41,34 @@ void main() {
       expect(result, Right(versionInfoModel));
     });
 
-
-  });
-
-  group('getVersionInfo is Failure', () {
-    test('should return error message when the call to version info datasource is failure', () {
+    test('should return error message when the call to version info datasource is failure', () async {
       // arrange
       when(remoteDataSource.getVersionInfo())
           .thenThrow(ServerException());
 
       // act
-      final result = repository.getVersionInfo();
+      final result = await repository.getVersionInfo();
 
       // assert
-      expect(result, throwsA(ServerException()));
+      verify(remoteDataSource.getVersionInfo());
+      expect(result, equals(Left(ServerFailure())));
+    });
+  });
+
+  group('getVersionInfo is offline', () {
+    setUp(() {
+      when(networkInfo.isConnected)
+          .thenAnswer((_) async => false);
+    });
+
+    test('should return error message when the call to version info datasource is failure', () async {
+      // act
+      final result = await repository.getVersionInfo();
+
+      // assert
+      verifyNever(remoteDataSource.getVersionInfo());
+      verifyZeroInteractions(remoteDataSource);
+      expect(result, equals(Left(ServerFailure())));
     });
   });
 }
