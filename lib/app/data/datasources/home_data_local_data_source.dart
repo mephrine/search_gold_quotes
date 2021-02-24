3162b39885a879f3a3ba15d7bc75c56ee9369ac3
Mephrine <dthwrld@gmail.com>
2021-02-24 21:50:39 +0900

    HomeDataLocalDataSource cache정보 가져오기

--- lib/app/data/datasources/home_data_local_data_source.dart
+++ lib/app/data/datasources/home_data_local_data_source.dart
@@ -1,6 +1,30 @@
+import 'dart:convert';
 
+import 'package:meta/meta.dart';
 import 'package:search_gold_quotes/app/data/models/home_data_model.dart';
+import 'package:search_gold_quotes/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-abstract class  HomeDataLocalDataSource {
+abstract class HomeDataLocalDataSource {
   Future<HomeDataModel> getLastHomeData();
+}
+
+const String CACHED_MAIN_DATA = "CACHED_MAIN_DATA";
+class HomeDataLocalDataSourceImpl extends HomeDataLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+
+  HomeDataLocalDataSourceImpl({
+    @required this.sharedPreferences
+  });
+
+  @override
+  Future<HomeDataModel> getLastHomeData() {
+    final jsonString = sharedPreferences.getString(CACHED_MAIN_DATA);
+    if(jsonString != null) {
+      return Future.value(HomeDataModel.fromJSON(json.decode(jsonString)));
+    }
+    throw CacheException();
+  }
+
 }
---
+++ test/app/data/datasources/home_data_local_data_source_test.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:search_gold_quotes/app/data/datasources/home_data_local_data_source.dart';
+import 'package:search_gold_quotes/app/data/models/gold_model.dart';
+import 'package:search_gold_quotes/app/data/models/home_data_model.dart';
+import 'package:search_gold_quotes/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  HomeDataLocalDataSource localDataSource;
+  MockSharedPreferences sharedPreferences;
+  setUp(() {
+    sharedPreferences = MockSharedPreferences();
+    localDataSource = HomeDataLocalDataSourceImpl(sharedPreferences: sharedPreferences);
+  });
+
+  group('HomeDataLocalDataSource', () {
+    final goldList = [
+      GoldModel(jewelryType: 'G', goldDate: '2021-02-10', goldPurity: '100', goldPriceType: 'W', goldPrice: '100000'),
+      GoldModel(jewelryType: 'G', goldDate: '2021-02-09', goldPurity: '70', goldPriceType: 'W', goldPrice: '90000'),
+      GoldModel(jewelryType: 'G', goldDate: '2021-02-08', goldPurity: '100', goldPriceType: 'W', goldPrice: '95000'),
+    ];
+    final homeDataModel = HomeDataModel(famousQuotes: 'Gold is God', referenceSiteName: 'https://www.naver.com', goldList: goldList);
+      test('should get last home data when cache is exist', () async {
+          // arrange
+          when(sharedPreferences.getString(CACHED_MAIN_DATA))
+              .thenReturn(fixture('home_data.json'));
+          // act
+        final result = await localDataSource.getLastHomeData();
+
+          // assert
+          expect(result, homeDataModel);
+       });
+
+      test('should return CacheException when cache is empty', () async {
+        // arrange
+        when(sharedPreferences.getString(any))
+            .thenReturn(null);
+        // act
+        final call = localDataSource.getLastHomeData;
+        // assert
+        expect(() => call(), throwsA(isInstanceOf<CacheException>()));
+
+      });
+  });
+}
