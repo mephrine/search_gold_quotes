1dfd2b432c5819739ab3f497d22c6495b5155c01
Mephrine <dthwrld@gmail.com>
2021-02-28 16:24:18 +0900

    VideoItems 모델 생성

---
+++ lib/app/data/models/video_items_model.dart
@@ -0,0 +1,23 @@
+import 'package:search_gold_quotes/app/domain/entities/video_items.dart';
+
+class VideoItemListModel extends VideoList {
+  VideoItemListModel.fromJson(Map<String, dynamic> jsonMap) {
+
+  }
+
+  Map<String, dynamic> toJson(String jsonString) {
+
+  }
+}
+
+
+class VideoItemModel extends VideoItem {
+  VideoItemModel.fromJson(Map<String, dynamic> jsonMap) {
+
+  }
+
+  Map<String, dynamic> toJson(String jsonString) {
+
+  }
+}
+
