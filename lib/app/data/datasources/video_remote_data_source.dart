0135f1008376cef66987f1956824bbce036b8645
Mephrine <dthwrld@gmail.com>
2021-02-28 23:20:51 +0900

    VideoRemoteDataSourceImpl 작성

--- lib/app/data/datasources/video_remote_data_source.dart
+++ lib/app/data/datasources/video_remote_data_source.dart
@@ -1,13 +1,31 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
 import 'package:search_gold_quotes/app/data/models/video_items_model.dart';
+import 'package:search_gold_quotes/core/error/exceptions.dart';
+import 'package:search_gold_quotes/core/values/strings.dart';
 
 abstract class VideoRemoteDataSource {
   Future<VideoListModel> getVideoList();
 }
 
 class VideoRemoteDataSourceImpl extends VideoRemoteDataSource {
+  final Dio httpClient;
+
+  VideoRemoteDataSourceImpl({@required this.httpClient});
+
   @override
   Future<VideoListModel> getVideoList() async {
-    return null;
+    final response = await httpClient.get(apiURL);
+    if (response.statusCode == 200) {
+      try {
+        return VideoListModel.fromJson(jsonDecode(response.data));
+      } catch (exception) {
+        throw ParseException();
+      }
+    } else {
+      throw ServerException();
+    }
   }
-
 }
---
+++ test/app/data/datasources/video_remote_data_source_test.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:search_gold_quotes/app/data/datasources/video_remote_data_source.dart';
+import 'package:search_gold_quotes/app/data/models/video_items_model.dart';
+import 'package:search_gold_quotes/core/error/exceptions.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements Dio {}
+
+void main() {
+  MockHttpClient httpClient;
+  VideoRemoteDataSourceImpl remoteDataSource;
+
+  setUp(() {
+    httpClient = MockHttpClient();
+    remoteDataSource = VideoRemoteDataSourceImpl(httpClient: httpClient);
+  });
+
+  group('VideoRemoteDataSourceImpl', () {
+    VideoListModel videoList;
+    setUp(() {
+      videoList = VideoListModel(itemList: [
+        VideoItemModel(title: 'Video1', subTitle: 'SubTitle1', linkURL: 'https://www.naver.com', imagePath: 'https://lottiefiles.com/10203-gold'),
+        VideoItemModel(title: 'Video2', subTitle: 'SubTitle2', linkURL: 'https://www.daum.net', imagePath: 'https://lottiefiles.com/10204-gold'),
+        VideoItemModel(title: 'Video3', subTitle: 'SubTitle3', linkURL: 'https://www.nate.com', imagePath: 'https://lottiefiles.com/10205-gold')
+      ]);
+    });
+
+    test('StatusCode 가 200(성공)일 때, VideoListModel 반환', () async {
+      // arrange
+      when(httpClient.get(any))
+          .thenAnswer((_) async => Response(statusCode: 200, data: fixture('video.json')));
+      // act
+      final result = await remoteDataSource.getVideoList();
+
+      // assert
+      expect(result, videoList);
+    });
+
+    test('StatusCode 가 200(성공)이고, response data가 null을 반환했을 때, ParseException 발생', () async {
+      // arrange
+      when(httpClient.get(any))
+          .thenAnswer((_) async => Response(statusCode: 200, data: null));
+      // act
+      final call = remoteDataSource.getVideoList();
+
+      // assert
+      expect(() => call, throwsA(isInstanceOf<ParseException>()));
+    });
+
+    test('StatusCode 가 500(실패)일 때, ServerException 발생', () async {
+      // arrange
+      when(httpClient.get(any))
+          .thenAnswer((_) async => Response(statusCode: 500, statusMessage: '일시적인 에러'));
+      // act
+      final call = remoteDataSource.getVideoList();
+
+      // assert
+      expect(() => call, throwsA(isInstanceOf<ServerException>()));
+    });
+  });
+}
