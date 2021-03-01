2b46e5afc5269254d7ab0eea030b9bee98876f4b
Mephrine <dthwrld@gmail.com>
2021-03-01 17:56:00 +0900

    video bloc test 파일 생성

---
+++ test/app/presentation/pages/main/video/bloc/video_bloc_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {
+
+  });
+
+  group('', () {
+    test('should', () async {
+        // arrange
+
+        // act
+
+        // assert
+     });
+  });
+}
