073056d5e463106551c11cf531642f2944377e9a
Mephrine <dthwrld@gmail.com>
2021-03-02 14:56:25 +0900

    VideoListWidget 작업

--- lib/app/presentation/pages/main/video/video_view.dart
+++ lib/app/presentation/pages/main/video/video_view.dart
@@ -1,12 +1,45 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:search_gold_quotes/app/presentation/pages/main/video/video/video_bloc.dart';
+import 'package:search_gold_quotes/core/di/injection_container.dart';
 
 class VideoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => container<VideoBloc>(),
+      child: VideoListWidget(),
+    );
+  }
+}
+
+class VideoListWidget extends StatefulWidget {
+  @override
+  _VideoListWidget createState() => _VideoListWidget();
+}
+
+class _VideoListWidget extends State<VideoListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return VideoItemWidget();
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    );
+  }
+}
+
+class VideoItemWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('VideoView'),
-      ),
+      height: 100,
     );
   }
 }
