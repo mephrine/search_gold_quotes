2e1e4e673e86f6b23f0adeccd53d952aa4c4e6d1
Mephrine <dthwrld@gmail.com>
2021-03-16 23:31:15 +0900

    오류 수정

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -14,13 +14,6 @@ import 'package:search_gold_quotes/app/domain/repositories/video_repository.dart
 import 'package:search_gold_quotes/app/domain/usecases/get_home_data.dart';
 import 'package:search_gold_quotes/app/domain/usecases/get_version_info.dart';
 import 'package:search_gold_quotes/app/domain/usecases/get_video_list.dart';
-import 'package:search_gold_quotes/app/number_trivia/data/datasources/number_trivia_local_data_source.dart';
-import 'package:search_gold_quotes/app/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
-import 'package:search_gold_quotes/app/number_trivia/data/repositories/number_trivia_repository_impl.dart';
-import 'package:search_gold_quotes/app/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:search_gold_quotes/app/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
-import 'package:search_gold_quotes/app/number_trivia/domain/usecases/get_random_number_trivia.dart';
-import 'package:search_gold_quotes/app/number_trivia/presentation/number_trivia/number_trivia_bloc.dart';
 import 'package:search_gold_quotes/app/presentation/pages/intro/bloc/bloc.dart';
 import 'package:search_gold_quotes/app/presentation/pages/main/home/home/home_bloc.dart';
 import 'package:search_gold_quotes/app/presentation/pages/main/video/video/video_bloc.dart';
@@ -37,94 +30,59 @@ Future<void> init() async {
   //! Features - VersionInfo
 
   // Bloc
-  container.registerFactory(() =>
-    SplashBloc(versionInfo: container())
-  );
+  container.registerFactory(() => SplashBloc(versionInfo: container()));
 
-  container.registerFactory(() =>
-    HomeBloc(homeInfo: container())
-  );
+  container.registerFactory(() => HomeBloc(homeInfo: container()));
 
-  container.registerFactory(() =>
-      VideoBloc(videoListUsecase: container())
-  );
+  container.registerFactory(() => VideoBloc(videoListUsecase: container()));
 
   // Use cases
-  container.registerLazySingleton(() => GetVersionInfo(repository: container()));
-  container.registerLazySingleton(() => GetHomeInfo(homeRepository: container()));
+  container
+      .registerLazySingleton(() => GetVersionInfo(repository: container()));
+  container
+      .registerLazySingleton(() => GetHomeInfo(homeRepository: container()));
   container.registerLazySingleton(() => GetVideoList(repository: container()));
 
   // Repository
   container.registerLazySingleton<VersionRepository>(() =>
-    VersionRepositoryImpl(remoteDataSource: container(), networkInfo: container())
-  );
-  container.registerLazySingleton<HomeRepository>(() =>
-      HomeRepositoryImpl(remoteDataSource: container(), networkInfo: container(), localDataSource: container())
-  );
+      VersionRepositoryImpl(
+          remoteDataSource: container(), networkInfo: container()));
+  container.registerLazySingleton<HomeRepository>(() => HomeRepositoryImpl(
+      remoteDataSource: container(),
+      networkInfo: container(),
+      localDataSource: container()));
   container.registerLazySingleton<VideoRepository>(() =>
-      VideoRepositoryImpl(dataSource: container(), networkInfo: container())
-  );
+      VideoRepositoryImpl(dataSource: container(), networkInfo: container()));
 
   // DataSource
-  container.registerLazySingleton<VersionRemoteDataSource>(() =>
-      VersionRemoteDataSourceImpl(httpClient: container())
-  );
-  container.registerLazySingleton<HomeDataRemoteDataSource>(() =>
-      HomeDataRemoteDataSourceImpl(httpClient: container())
-  );
-  container.registerLazySingleton<HomeDataLocalDataSource>(() =>
-    HomeDataLocalDataSourceImpl(sharedPreferences: container())
-  );
-  container.registerLazySingleton<VideoRemoteDataSource>(() =>
-      VideoRemoteDataSourceImpl(httpClient: container())
-  );
-
-  //! Featurs - NumberTrivia
-
-  // Bloc
-  container.registerFactory(() =>
-      NumberTriviaBloc(
-        concrete: container(),
-        random: container(),
-        inputConverter: container(),
-      ));
-
-  // Use cases
-  container.registerLazySingleton(() => GetConcreteNumberTrivia(container()));
-  container.registerLazySingleton(() => GetRandomNumberTrivia(container()));
-
-  // Repository
-  container.registerLazySingleton<NumberTriviaRepository>(
-          () =>
-          NumberTriviaRepositoryImpl(
-            remoteDataSource: container(),
-            localDataSource: container(),
-            networkInfo: container(),
-          ));
-
-  // Data Sources
-  container.registerLazySingleton<NumberTriviaRemoteDataSource>(() =>
-      NumberTriviaRemoteDataSourceImpl(httpClient: container(),));
-  container.registerLazySingleton<NumberTriviaLocalDataSource>(() =>
-      NumberTriviaLocalDataSourceImpl(preferences: container(),));
-
+  container.registerLazySingleton<VersionRemoteDataSource>(
+      () => VersionRemoteDataSourceImpl(httpClient: container()));
+  container.registerLazySingleton<HomeDataRemoteDataSource>(
+      () => HomeDataRemoteDataSourceImpl(httpClient: container()));
+  container.registerLazySingleton<HomeDataLocalDataSource>(
+      () => HomeDataLocalDataSourceImpl(sharedPreferences: container()));
+  container.registerLazySingleton<VideoRemoteDataSource>(
+      () => VideoRemoteDataSourceImpl(httpClient: container()));
 
   //! External
   final sharePreferences = await SharedPreferences.getInstance();
   container.registerLazySingleton(() => sharePreferences);
   container.registerLazySingleton(() => Dio(BaseOptions(
-      baseUrl: constants.apiURL + constants.apiVersion,
-      connectTimeout: 5000,
-      receiveTimeout: 3000,
-      headers: { "api-key": "goldnawa!@#", 'Content-Type': 'application/json' })));
+          baseUrl: constants.apiURL + constants.apiVersion,
+          connectTimeout: 5000,
+          receiveTimeout: 3000,
+          headers: {
+            "api-key": "goldnawa!@#",
+            'Content-Type': 'application/json'
+          })));
   container.registerLazySingleton(() => Connectivity());
-
 
   //! Core
   container.registerLazySingleton(() => InputConverter());
-  container.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(container()));
-  container.registerLazySingleton(() => ThemeNotifier(preferences: container()));
-
+  container
+      .registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(container()));
+  container
+      .registerLazySingleton(() => ThemeNotifier(preferences: container()));
 }
 
 void initFeatures() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:search_gold_quotes/app/number_trivia/presentation/pages/number_trivia_page.dart';
 import 'core/di/injection_container.dart' as di;
 import 'core/presentation/routes/router.gr.dart';
 import 'core/theme/theme_notifier.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();  // 비동기로 진행 시, 반드시 추가해야함.
+  WidgetsFlutterBinding.ensureInitialized(); // 비동기로 진행 시, 반드시 추가해야함.
   await di.init();
   runApp(Main());
 }
@@ -27,8 +26,7 @@ class Main extends StatelessWidget {
             routeInformationParser: _appRouter.defaultRouteParser(),
           ),
         );
-      }
-      ),
+      }),
     );
   }
 }
