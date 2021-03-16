34572b6ca52666ddf44c19cad0077f3ddba84203
Mephrine <dthwrld@gmail.com>
2021-03-16 23:24:01 +0900

    fixture 추가 및 numberTrivia소스 정리

--- lib/app/number_trivia/domain/repositories/number_trivia_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:search_gold_quotes/app/number_trivia/domain/entities/number_trivia.dart';
-import 'package:search_gold_quotes/core/error/failures.dart';
-
-// 해당 부분이 Dart에서는 인터페이스
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
-}
